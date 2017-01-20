d814611928582edc8de18ed8ac428a00f57ab3fd
Abokor <abokor_88@hotmail.com>
2017-01-20 22:07:26 +0300

    Update of the Layout and the include pages.

--- Welcome.php
+++ Welcome.php
@@ -1,14 +1,25 @@
-
 <?php
-  include ('includes/header.php');
-  if ($_SESSION['userid'] == '') {
-    header('Location: index.php');
-    exit();
-  }
+include ('includes/init.php');
+
+if ($_SESSION['userid'] == '') {
+  header('Location: index.php');
+  exit();
+}
 ?>
 
-<div class="welcome-content">
-  <h1> This is the Welcome page!!! </h1>
-</div>
+<!-- This is to import the main header -->
+<?php include ('includes/header.php');?>
+<!-- This is the end of the import of the main header -->
+
+<!-- This is to import the Main Side Bar Navigation -->
+<?php include ('includes/side_bar_navigation.php'); ?>
+
+<!-- start of the content wrapper -->
+	<div class="content-wrapper">
+      <div class="welcome-content">
+        <h1>This is the welcome page</h1>
+      </div>
+  </div>
+<!-- .content-wrapper -->
 
 <?php include ('includes/footer.php'); ?>
---
+++ includes/side_bar_navigation.php
@@ -0,0 +1,85 @@
+
+<!-- This is the beginning of the main side bar navigation -->
+<main class="cd-main-content">
+
+<nav class="cd-side-nav" >
+  <ul>
+    <li class="cd-label" style="font-size:17px">DASHBOARD</li>
+    <li class="has-children overview">
+      <a href="#0" style="font-size:15px">Reports</a>
+      <ul>
+        <li><a href="#0" style="font-size:15px">Overall</a></li>
+        <li><a href="#0" style="font-size:15px">Stamps</a></li>
+        <li><a href="#0" style="font-size:15px">Cheques</a></li>
+        <li><a href="#0" style="font-size:15px">Refunds</a></li>
+        <li><a href="#0" style="font-size:15px">Loans</a></li>
+        <li><a href="#0" style="font-size:15px">Expenses</a></li>
+      </ul>
+    </li>
+    <li class="has-children notifications">
+      <a href="#0" style="font-size:15px">Notifications<span class="count" style="font-size:15px">3</span></a>
+
+      <ul>
+        <li><a href="#0" style="font-size:15px">All Notifications</a></li>
+        <li><a href="#0" style="font-size:15px">Tellers</a></li>
+        <li><a href="#0" style="font-size:15px">Chief Cashiers</a></li>
+      </ul>
+    </li>
+
+    <li class="has-children comments">
+      <a href="opening_balance.php" style="font-size:15px">Opening Balance</a>
+
+    </li>
+  </ul>
+
+  <ul>
+    <li class="cd-label" style="font-size:17px">CASH IN</li>
+    <li class="has-children bookmarks">
+      <a href="remittance.php" style="font-size:15px">Remittance</a>
+      <!-- Under Remittance
+      <ul>
+        <li><a href="#0">All Bookmarks</a></li>
+        <li><a href="#0">Edit Bookmark</a></li>
+        <li><a href="#0">Import Bookmark</a></li>
+      </ul> -->
+
+    </li>
+    <li> <a href="#0" style="font-size:15px">Cheques</a> </li>
+    <li class="has-children images">
+      <a href="#0" style="font-size:15px">Stamps</a>
+      <!--
+      <ul>
+        <li><a href="#0">All Images</a></li>
+        <li><a href="#0">Edit Image</a></li>
+      </ul>-->
+    </li>
+  </ul>
+
+  <ul>
+    <li class="cd-label" style="font-size:17px">CASH OUT</li>
+    <li class="has-children bookmarks">
+      <a href="#0" style="font-size:15px">Payments</a>
+    </li>
+    <li>
+      <a href="#0" style="font-size:15px">Refunds</a>
+    </li>
+
+    <li>
+      <a href="#0" style="font-size:15px">Loans</a>
+    </li>
+
+    <li>
+      <a href="#0" style="font-size:15px">Expenses</a>
+    </li>
+  </ul>
+
+      <ul>
+        <li class="cd-label" style="font-size:17px">Action</li>
+        <li></li>
+        <li class="action-btn"><a href="#0" style="font-size:13px">+ USERS</a></li>
+      </ul>
+
+</nav>
+
+<!-- This is the end of the main side bar navigation -->
+</main> <!-- .cd-main-content -->
